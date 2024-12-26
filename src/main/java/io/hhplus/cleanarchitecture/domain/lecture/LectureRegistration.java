2dc6efa047d2757302b0b875fdb6841ba43cf7dc
ohhyoungseok <gudtjr1350@gmail.com>
2024-12-27 03:53:59 +0900

    chore : LectureRegistration 객체에 unique 제약조건 추가하여 완전히 방지하도록 처리

--- src/main/java/io/hhplus/cleanarchitecture/domain/lecture/LectureRegistration.java
+++ src/main/java/io/hhplus/cleanarchitecture/domain/lecture/LectureRegistration.java
@@ -12,10 +12,10 @@ import org.hibernate.annotations.Where;
 @NoArgsConstructor
 @Entity
 @Table(name = "lecture_registration"
-            /*,uniqueConstraints = {
+            ,uniqueConstraints = {
                     @UniqueConstraint(name = "unique_lecture_user",
                             columnNames = {"lecture_id", "user_id"})
-            }*/
+            }
         )
 @Where(clause = "deleted_at IS NULL")
 public class LectureRegistration extends BaseEntity {
